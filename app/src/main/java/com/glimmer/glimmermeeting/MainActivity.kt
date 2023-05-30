a2abdcf671ce983cc8fb4ebf989a3605ebab6b9a
Meng Pei <2398696309@qq.com>
2023-05-30 10:51:51 +0800

    登录控制逻辑

--- app/src/main/java/com/glimmer/glimmermeeting/MainActivity.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/MainActivity.kt
@@ -2,8 +2,17 @@ package com.glimmer.glimmermeeting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity(R.layout.main_layout) {
+
+    val okHttpClient = OkHttpClient()
+    val moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
--- app/src/main/java/com/glimmer/glimmermeeting/userentry/LoginFragment.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/userentry/LoginFragment.kt
@@ -1,6 +1,11 @@
 package com.glimmer.glimmermeeting.userentry
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.os.Message.obtain
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -8,7 +13,15 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.glimmer.glimmermeeting.MainActivity
 import com.glimmer.glimmermeeting.R
+import com.squareup.moshi.JsonClass
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.FormBody
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 class LoginFragment : Fragment(R.layout.login_layout) {
 
@@ -16,18 +29,69 @@ class LoginFragment : Fragment(R.layout.login_layout) {
     private lateinit var passwordView: EditText
     private lateinit var loginButton: Button
 
-    private fun checkInput(username: String, password: String): Boolean {
+    @JsonClass(generateAdapter = true)
+    data class LoginJson(
+        var message: String,
+        var token: String?
+    )
+
+    private fun checkInput(username: String, password: String) {
         if (username == "") {
             Toast.makeText(this.context, "用户名为空", Toast.LENGTH_SHORT).show()
-            return false
+            return
         }
 
         if (password == "") {
             Toast.makeText(this.context, "密码为空", Toast.LENGTH_SHORT).show()
-            return false
+            return
         }
 
-        return true
+        postLogin(username, password)
+    }
+
+    private val loginSuccessHandler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+
+            Toast.makeText(context, msg.data.getString("message"), Toast.LENGTH_SHORT).show()
+
+            if (msg.data.getBoolean("state")) {
+                Navigation.findNavController(requireView()).navigate(R.id.userentryFragment_to_appFragment)
+            }
+        }
+    }
+
+    private fun postLogin(username: String, password: String) {
+        val loginJsonAdapter = MainActivity().moshi.adapter(LoginJson::class.java)
+
+        val loginBody = FormBody.Builder()
+            .add("username", username)
+            .add("password", password)
+            .build()
+        val loginRequest = Request.Builder()
+            .url("http://api.mcyou.cc:2023/login")
+            .post(loginBody)
+            .build()
+
+        MainActivity().okHttpClient.newCall(loginRequest).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("Login", e.printStackTrace().toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val loginState = response.isSuccessful
+                val loginJson = loginJsonAdapter.fromJson(response.body!!.string())
+
+                val loginMessage = obtain()
+                val loginBundle = Bundle()
+                loginBundle.putBoolean("state", loginState)
+                loginBundle.putString("message", loginJson!!.message)
+                loginBundle.putString("token", loginJson.token)
+                loginMessage.data = loginBundle
+
+                loginSuccessHandler.sendMessage(loginMessage)
+            }
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -39,9 +103,7 @@ class LoginFragment : Fragment(R.layout.login_layout) {
             val username = usernameView.text.toString()
             val password = passwordView.text.toString()
 
-            if (checkInput(username, password)) {
-                Navigation.findNavController(view).navigate(R.id.userentryFragment_to_appFragment)
-            }
+            checkInput(username, password)
         }
     }
 }
