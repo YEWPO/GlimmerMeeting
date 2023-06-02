1c10e3b8d33e20a907dc4c94db7be87412b2e0d2
Meng Pei <2398696309@qq.com>
2023-06-02 10:55:42 +0800

    更换了背景图片和图标

--- app/src/main/java/com/glimmer/glimmermeeting/app/AppFragment.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/app/AppFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.glimmer.glimmermeeting.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,9 +13,11 @@ class AppFragment : Fragment(R.layout.app_layout) {
     val createdArgs: AppFragmentArgs by navArgs()
 
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var userToken: String
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         bottomNavigationView = view.findViewById(R.id.appBottomNavView)
+        userToken = createdArgs.token
 
         bottomNavigationView.setupWithNavController(childFragmentManager.findFragmentById(R.id.app_fragment_container)!!.findNavController())
     }
