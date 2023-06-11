c65da7186d9e431a65d257e14077551b511217f4
Meng Pei <2398696309@qq.com>
2023-06-11 18:05:31 +0800

    个人头像框

--- app/src/main/java/com/glimmer/glimmermeeting/app/SettingFragment.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/app/SettingFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.glimmer.glimmermeeting.R
@@ -11,9 +12,15 @@ import com.glimmer.glimmermeeting.R
 class SettingFragment : Fragment(R.layout.setting_layout) {
     private lateinit var logoutButton: Button
 
+    private lateinit var settingUserAccount: TextView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         logoutButton = view.findViewById(R.id.logoutButton)
+        settingUserAccount = view.findViewById(R.id.settingUserAccount)
+
         val sharedPreferences = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+
+        settingUserAccount.text = sharedPreferences.getString("loginToken", "null")
 
         logoutButton.setOnClickListener {
             with(sharedPreferences.edit()) {
