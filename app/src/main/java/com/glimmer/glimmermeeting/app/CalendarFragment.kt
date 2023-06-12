ccdc1e0fe0103e7aaa54e3c8643422373bde9465
Meng Pei <2398696309@qq.com>
2023-06-12 22:45:51 +0800

    Fragment Compose

--- app/src/main/java/com/glimmer/glimmermeeting/app/CalendarFragment.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/app/CalendarFragment.kt
@@ -1,7 +1,26 @@
 package com.glimmer.glimmermeeting.app
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
 import androidx.fragment.app.Fragment
 import com.glimmer.glimmermeeting.R
+import androidx.compose.ui.platform.ComposeView
 
-class CalendarFragment : Fragment(R.layout.calendar_layout) {
+class CalendarFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.calendar_layout, container, false).apply {
+            findViewById<ComposeView>(R.id.calendarComposeView).apply { 
+                setContent { 
+                    Text(text = "hello")
+                }
+            }
+        }
+    }
 }
