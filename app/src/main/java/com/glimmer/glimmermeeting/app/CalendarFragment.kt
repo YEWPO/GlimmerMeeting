db9ce03742dac6c7ca74a43774aab3b043d28f13
Meng Pei <2398696309@qq.com>
2023-06-12 23:06:54 +0800

    周日历

--- app/src/main/java/com/glimmer/glimmermeeting/app/CalendarFragment.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/app/CalendarFragment.kt
@@ -4,10 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.fragment.app.Fragment
 import com.glimmer.glimmermeeting.R
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.kizitonwose.calendar.compose.WeekCalendar
+import com.kizitonwose.calendar.compose.weekcalendar.rememberWeekCalendarState
+import com.kizitonwose.calendar.core.WeekDay
+import com.kizitonwose.calendar.core.atStartOfMonth
+import com.kizitonwose.calendar.core.daysOfWeek
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.Locale
 
 class CalendarFragment : Fragment() {
     override fun onCreateView(
@@ -18,9 +40,68 @@ class CalendarFragment : Fragment() {
         return inflater.inflate(R.layout.calendar_layout, container, false).apply {
             findViewById<ComposeView>(R.id.calendarComposeView).apply { 
                 setContent { 
-                    Text(text = "hello")
+                    MainScreen()
                 }
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreen() {
+    val currentDate = remember {
+        LocalDate.now()
+    }
+    val currentMonth = remember {
+        YearMonth.now()
+    }
+    val startDate = remember {
+        currentMonth.atStartOfMonth()
+    }
+    val endDate = remember {
+        currentMonth.atEndOfMonth()
+    }
+    val dayOfWeek = remember {
+        daysOfWeek()
+    }
+
+    val state = rememberWeekCalendarState(
+        startDate,
+        endDate,
+        currentDate,
+        dayOfWeek.first()
+    )
+    
+    Column {
+        DaysOfWeekTitle(daysOfWeek = dayOfWeek)
+        WeekCalendar(
+            state = state,
+            dayContent = { Day(it) }
+        )
+    }
+}
+
+@Composable
+fun Day(day: WeekDay) {
+    Box(
+        modifier = Modifier
+            .aspectRatio(1f), // This is important for square sizing!
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = day.date.dayOfMonth.toString())
+    }
+}
+
+@Composable
+fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        for (dayOfWeek in daysOfWeek) {
+            Text(
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center,
+                text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
+            )
         }
     }
 }
