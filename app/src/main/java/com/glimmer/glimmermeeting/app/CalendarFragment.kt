ffebe7b28765f189770d7282dd201eeec18d05d0
Meng Pei <2398696309@qq.com>
2023-06-14 00:04:21 +0800

    周日历UI

--- app/src/main/java/com/glimmer/glimmermeeting/app/CalendarFragment.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/app/CalendarFragment.kt
@@ -4,30 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.fragment.app.Fragment
 import com.glimmer.glimmermeeting.R
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kizitonwose.calendar.compose.WeekCalendar
 import com.kizitonwose.calendar.compose.weekcalendar.rememberWeekCalendarState
-import com.kizitonwose.calendar.core.WeekDay
 import com.kizitonwose.calendar.core.atStartOfMonth
 import com.kizitonwose.calendar.core.daysOfWeek
-import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
 import java.util.Locale
 
@@ -39,7 +49,7 @@ class CalendarFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.calendar_layout, container, false).apply {
             findViewById<ComposeView>(R.id.calendarComposeView).apply { 
-                setContent { 
+                setContent {
                     MainScreen()
                 }
             }
@@ -65,42 +75,69 @@ fun MainScreen() {
     val dayOfWeek = remember {
         daysOfWeek()
     }
+    var selection by remember {
+        mutableStateOf(currentDate)
+    }
 
     val state = rememberWeekCalendarState(
-        startDate,
-        endDate,
-        currentDate,
-        dayOfWeek.first()
+        startDate = startDate,
+        endDate = endDate,
+        firstVisibleWeekDate = currentDate
     )
     
     Column {
-        DaysOfWeekTitle(daysOfWeek = dayOfWeek)
+        Text(text = YearMonth.now().toString())
         WeekCalendar(
+            modifier = Modifier
+                .background(color = Color.White),
             state = state,
-            dayContent = { Day(it) }
+            dayContent = { day ->
+                Day(date = day.date, isSelected = selection == day.date) { clicked ->
+                    if (selection != clicked) {
+                        selection = clicked
+                    }
+                }
+            }
         )
     }
 }
 
-@Composable
-fun Day(day: WeekDay) {
-    Box(
-        modifier = Modifier
-            .aspectRatio(1f), // This is important for square sizing!
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = day.date.dayOfMonth.toString())
-    }
-}
+private val dateFormatter = DateTimeFormatter.ofPattern("dd")
 
 @Composable
-fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        for (dayOfWeek in daysOfWeek) {
+private fun Day(date: LocalDate, isSelected: Boolean, onClick: (LocalDate) -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable { onClick(date) },
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            modifier = Modifier.padding(vertical = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+        ) {
             Text(
-                modifier = Modifier.weight(1f),
-                textAlign = TextAlign.Center,
-                text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
+                text = date.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
+                fontSize = 12.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Light,
+            )
+            Text(
+                text = dateFormatter.format(date),
+                fontSize = 14.sp,
+                color = if (isSelected) Color(0xff0099ff) else Color.Black,
+                fontWeight = FontWeight.Bold,
+            )
+        }
+        if (isSelected) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(5.dp)
+                    .background(Color(0xff0099ff))
+                    .align(Alignment.BottomCenter),
             )
         }
     }
