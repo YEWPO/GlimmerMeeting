85268f417055289ee50763a0d019dca170b6e39d
Meng Pei <2398696309@qq.com>
2023-06-19 11:06:32 +0800

    会议列表布局加载到主页面中

--- app/src/main/java/com/glimmer/glimmermeeting/app/HomeFragment.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/app/HomeFragment.kt
@@ -3,16 +3,44 @@ package com.glimmer.glimmermeeting.app
 import android.os.Bundle
 import android.view.View
 import android.widget.ListView
+import android.widget.SimpleAdapter
 import androidx.fragment.app.Fragment
 import com.glimmer.glimmermeeting.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment(R.layout.home_layout) {
-    private lateinit var myMeetingList: ListView
+    private lateinit var myMeetingListView: ListView
     private lateinit var addActionButton: FloatingActionButton
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        myMeetingList = view.findViewById(R.id.meetingList)
+        myMeetingListView = view.findViewById(R.id.meetingList)
         addActionButton = view.findViewById(R.id.addActionButton)
+
+        var myMeetingInfo = mutableListOf(
+            mapOf(
+                "weekdayName" to "周日",
+                "date" to "6月25日",
+                "roomInfo" to "testRoom",
+                "meetingTitle" to "testTitle",
+                "meetingTime" to "19:00-20:00"
+            ),
+            mapOf(
+                "weekdayName" to "周日",
+                "date" to "6月25日",
+                "roomInfo" to "testRoom",
+                "meetingTitle" to "testTitle",
+                "meetingTime" to "19:00-20:00"
+            )
+        )
+
+        val simpleAdapter = SimpleAdapter(
+            requireContext(),
+            myMeetingInfo,
+            R.layout.meeting_info_layout,
+            arrayOf("weekdayName", "date", "roomInfo", "meetingTitle", "meetingTime"),
+            intArrayOf(R.id.weekdayName, R.id.dateText, R.id.roomInfoText, R.id.meetingTitleText, R.id.meetingTimeText)
+        )
+
+        myMeetingListView.adapter = simpleAdapter
     }
 }
