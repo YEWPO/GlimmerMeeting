fa9f037408c4bdfda1f5e04799e7fc983be64233
Meng Pei <2398696309@qq.com>
2023-06-19 10:21:29 +0800

    主页框架

--- app/src/main/java/com/glimmer/glimmermeeting/app/HomeFragment.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/app/HomeFragment.kt
@@ -1,7 +1,18 @@
 package com.glimmer.glimmermeeting.app
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.glimmer.glimmermeeting.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment(R.layout.home_layout) {
+    private lateinit var myMeetingList: ListView
+    private lateinit var addActionButton: FloatingActionButton
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        myMeetingList = view.findViewById(R.id.meetingList)
+        addActionButton = view.findViewById(R.id.addActionButton)
+    }
 }
