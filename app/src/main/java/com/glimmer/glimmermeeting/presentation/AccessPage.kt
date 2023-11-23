066d840dc40954c7a389d31f9e07e2cd1658d2e1
Meng Pei <2398696309@qq.com>
2023-11-23 22:03:00 +0800

    AccessPage Framework

---
+++ app/src/main/java/com/glimmer/glimmermeeting/presentation/AccessPage.kt
@@ -0,0 +1,18 @@
+package com.glimmer.glimmermeeting.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.glimmer.glimmermeeting.ui.theme.GlimmerMeetingTheme
+
+@Composable
+fun AccessPage() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AccessPagePreview() {
+    GlimmerMeetingTheme {
+        AccessPage()
+    }
+}
