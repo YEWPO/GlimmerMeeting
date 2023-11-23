cdf6a32ad8286df0d0880312b20542b9621e625d
Meng Pei <2398696309@qq.com>
2023-11-23 22:34:33 +0800

    AccessPage Title

--- app/src/main/java/com/glimmer/glimmermeeting/presentation/AccessPage.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/presentation/AccessPage.kt
@@ -1,12 +1,39 @@
 package com.glimmer.glimmermeeting.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.glimmer.glimmermeeting.R
 import com.glimmer.glimmermeeting.ui.theme.GlimmerMeetingTheme
 
 @Composable
 fun AccessPage() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(id = R.drawable.access_background),
+                contentScale = ContentScale.FillWidth,
+                alpha = 0.6f,
+                alignment = Alignment.TopCenter
+            )
+    ) {
+        AccessPageTitle()
+    }
 }
 
 @Preview(showBackground = true)
@@ -14,5 +41,23 @@ fun AccessPage() {
 fun AccessPagePreview() {
     GlimmerMeetingTheme {
         AccessPage()
+    }
+}
+
+@Composable
+fun AccessPageTitle() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "登录",
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(start = 30.dp, top = 50.dp)
+        )
     }
 }
