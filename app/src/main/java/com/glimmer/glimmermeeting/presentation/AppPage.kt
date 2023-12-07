f69e1885dba68874992f1c7abff093033cb55a33
Meng Pei <2398696309@qq.com>
2023-12-07 22:41:48 +0800

    Side Drawer UserInfo

--- app/src/main/java/com/glimmer/glimmermeeting/presentation/AppPage.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/presentation/AppPage.kt
@@ -1,9 +1,14 @@
 package com.glimmer.glimmermeeting.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalNavigationDrawer
@@ -12,9 +17,12 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.glimmer.glimmermeeting.ui.theme.GlimmerMeetingTheme
 import kotlinx.coroutines.launch
 
@@ -43,7 +51,7 @@ fun SideDrawer() {
     Column(
         modifier = Modifier
             .fillMaxHeight()
-            .width(260.dp)
+            .width(250.dp)
             .background(Color(0xFFFFFFFF))
     ) {
         UserInfo()
@@ -51,9 +59,32 @@ fun SideDrawer() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun SideDrawerPreview() {
+    GlimmerMeetingTheme {
+        SideDrawer()
+    }
+}
+
 @Composable
 fun UserInfo() {
-    Text(text = "Hello")
+    Column(
+        modifier = Modifier
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(5.dp)
+    ) {
+        Button(
+            modifier = Modifier
+                .size(80.dp)
+                .clip(CircleShape),
+            onClick = { }
+        ) { }
+        Column {
+            Text(text = "Name", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+            Text(text = "Apartment", color = Color(0xFF808080))
+        }
+    }
 }
 
 @Composable
