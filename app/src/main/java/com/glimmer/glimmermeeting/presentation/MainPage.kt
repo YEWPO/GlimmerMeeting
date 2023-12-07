daafdcec948cd0324df1b3e9ce24ff1379ca527b
Meng Pei <2398696309@qq.com>
2023-12-07 22:12:26 +0800

    Main Page TopBar

--- app/src/main/java/com/glimmer/glimmermeeting/presentation/MainPage.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/presentation/MainPage.kt
@@ -1,22 +1,61 @@
 package com.glimmer.glimmermeeting.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.glimmer.glimmermeeting.ui.theme.GlimmerMeetingTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainPage() {
-    Column {
-        CenterAlignedTopAppBar(
-            title = { Text(text = "Glimmer Meeting") }
-        )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        MainPageTopBar()
         FunctionCard()
         MyMeetingInfo()
+    }
+}
+
+@Composable
+fun MainPageTopBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Button(
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape),
+                onClick = { /*TODO*/ }
+            ) { }
+            Column {
+                Text(text = "Name", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+                Text(text = "Apartment", color = Color(0xff808080))
+            }
+        }
     }
 }
 
@@ -27,7 +66,6 @@ fun FunctionCard() {
 
 @Composable
 fun MyMeetingInfo() {
-
 }
 
 @Composable
