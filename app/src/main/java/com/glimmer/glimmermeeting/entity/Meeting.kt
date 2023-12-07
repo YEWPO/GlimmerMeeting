3c9782941e690d4fe9e6c75350165f3b9f8f1b2f
Meng Pei <2398696309@qq.com>
2023-12-07 20:20:54 +0800

    Main Page Framework

---
+++ app/src/main/java/com/glimmer/glimmermeeting/entity/Meeting.kt
@@ -0,0 +1,5 @@
+package com.glimmer.glimmermeeting.entity
+
+data class Meeting(
+    var theme: String
+)
--- app/src/main/java/com/glimmer/glimmermeeting/presentation/MainPage.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/presentation/MainPage.kt
@@ -1,19 +1,67 @@
 package com.glimmer.glimmermeeting.presentation
 
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.glimmer.glimmermeeting.ui.theme.GlimmerMeetingTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainPage() {
-    Text(text = "MainPage")
+    ModalNavigationDrawer(
+        drawerContent = {
+            SideDrawer()
+        }
+    ) {
+        /* Main Screen */
+    }
+}
+
+@Composable
+fun UserInfo() {
+
+}
+
+@Composable
+fun NavigationList() {
+
+}
+
+@Composable
+fun SideDrawer() {
+    Column {
+        UserInfo()
+        NavigationList()
+    }
+}
+
+@Composable
+fun FunctionCard() {
+
+}
+
+@Composable
+fun MeetingInfoCard() {
+
+}
+
+@Composable
+fun MeetingInfoDayCard() {
+
+}
+
+@Composable
+fun MyMeetingInfo() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MainPagePreview() {
-    Surface {
+    GlimmerMeetingTheme {
         MainPage()
     }
 }
