d0d935340d908ccccb0a5d3e8feb5f241d3b7799
Meng Pei <2398696309@qq.com>
2023-12-07 23:52:35 +0800

    Main Page FunctionButton Image

--- app/src/main/java/com/glimmer/glimmermeeting/presentation/MainPage.kt
+++ app/src/main/java/com/glimmer/glimmermeeting/presentation/MainPage.kt
@@ -1,27 +1,31 @@
 package com.glimmer.glimmermeeting.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.glimmer.glimmermeeting.R
 import com.glimmer.glimmermeeting.ui.theme.GlimmerMeetingTheme
 
 @Composable
@@ -72,37 +76,46 @@ fun FunctionCard() {
     ) {
         FunctionButton(
             functionDescriptor = "扫一扫",
+            functionPainter = painterResource(id = R.drawable.scanner),
             onFunctionClicked = {}
         )
         FunctionButton(
             functionDescriptor = "预定会议",
+            functionPainter = painterResource(id = R.drawable.more),
             onFunctionClicked = {}
         )
         FunctionButton(
             functionDescriptor = "历史会议",
+            functionPainter = painterResource(id = R.drawable.history),
             onFunctionClicked = {}
         )
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FunctionButton(
     functionDescriptor: String,
+    functionPainter: Painter,
     onFunctionClicked: () -> Unit
 ) {
     Column(
         modifier = Modifier
-            .width(100.dp)
-            .height(70.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .width(100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(6.dp)
     ) {
-        Button(
-            modifier = Modifier
-                .clip(RoundedCornerShape(2.dp)),
+        Surface (
             onClick = { onFunctionClicked() }
         ) {
+            Image(
+                modifier = Modifier
+                    .size(60.dp),
+                painter = functionPainter,
+                contentDescription = functionDescriptor
+            )
         }
-        Text(text = functionDescriptor)
+        Text(text = functionDescriptor, fontSize = 14.sp)
     }
 }
 
